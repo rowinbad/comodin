f7d5d953b1d3180b75274eea3973e5b5b361b020
pablo <pablo.villarreal@usach.cl>
2022-01-27 15:56:41 -0300

    Creacion controlador de rol y repectivas rutas

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $roles = Role::all();
+        if($roles == NULL){
+            return response()->json([
+                'respuesta' => 'No se encuentran roles',
+            ]);
+        }
+        return response($roles);
+        //return response()->json($roles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $roles = Role::findOrFail($id);
+        if($roles == NULL){
+            return response()->json([
+                'respuesta' => 'No existe el rol con ese id',
+            ]);
+        }
+        return response($roles);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta para la clase Role
+Route::get('/roles',[RoleController::class,'index']);
+Route::get('/roles/{id}',[RoleController::class,'show']);
+Route::post('/roles/create',[RoleController::class,'store']);
+Route::put('/roles/update/{id}',[RoleController::class,'update']);
+Route::delete('/roles/destroy/{id}',[RoleController::class,'destroy']);
